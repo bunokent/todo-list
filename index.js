09f1520077355aa4c48e69a115b2cea1cd760c60
sasukeCoder <119490147+kentbuno@users.noreply.github.com>
2024-07-25 10:01:06 +0800

    added a functionality to all the buttons and input

---
+++ index.js
@@ -0,0 +1,43 @@
+const addBtn = document.getElementById("add-btn");
+const addInput = document.getElementById("add-input");
+const mainEl = document.querySelector("main");
+const empty = document.querySelector(".empty-todo");
+let listCounter = 0;
+let delBtns = [];
+
+addBtn.addEventListener("click", addTodoItem);
+addInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") addTodoItem();
+});
+
+function createHTMl(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html.trim();
+  return template.content.firstElementChild;
+}
+
+function addTodoItem() {
+  if (addInput.value === "") return;
+  ++listCounter;
+  empty.classList.add("hide");
+
+  const template = createHTMl(`
+    <div class="container">
+      <div>
+        <input type="checkbox" id="checkbox${listCounter}"></input>
+        <label for="checkbox${listCounter}">
+          <span>${addInput.value}</span>
+        </label>
+      </div>
+      <button id="del-btn${listCounter}" class="del-btn"><img src="./assets/delete.png" /></button>
+    </div>
+    `);
+  mainEl.appendChild(template);
+  const currentBtn = document.getElementById(`del-btn${listCounter}`);
+  delBtns.push(currentBtn);
+  currentBtn.addEventListener("click", function () {
+    this.parentNode.remove();
+    listCounter--;
+    if (listCounter === 0) empty.classList.remove("hide");
+  });
+}
